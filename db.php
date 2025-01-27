dd243d3c9ca5aae30df1167edf0869e792a1b07d
estelle-dupuis <etelle.dupuis@laplateforme.io>
2025-01-27 19:32:28 +0100

    ajout: connexion a la base de données en PDO

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = "gestionaire de menu";
+
+try {
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+    exit();
+}
+
+?>
