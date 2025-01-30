3445033e517183dbef5456c72898133cef090598
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-01-30 11:06:42 +0100

    modif: changement de la langue en fr

--- modifier.php
+++ modifier.php
@@ -76,7 +76,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
